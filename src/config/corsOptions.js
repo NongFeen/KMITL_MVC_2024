5bf736cba565768b5d81c67fe0765923108dce57
Feen <kakfeenza@gmail.com>
2024-09-14 07:06:26 +0700

    finally fix(run on window not ubuntu), work on mobile

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,15 +1,7 @@
 const corsOptions = {
-    origin: (origin, callback) => {
-        const isLocalNetwork = origin && origin.startsWith('http://192.168.1.');
-        const isLocalhost = origin === 'http://localhost:5500' || origin === 'http://127.0.0.1:5500';
-        
-        if (isLocalNetwork || isLocalhost) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: '*',  
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     optionsSuccessStatus: 200
 };
-
 module.exports = corsOptions;
