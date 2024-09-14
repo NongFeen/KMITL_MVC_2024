06f40069760d6c57fe83ef6517fe897e2f903d6e
Feen <kakfeenza@gmail.com>
2024-09-14 11:45:24 +0700

    seperate mobile display and desktop display using hasTouchSupport()

--- src/public/scripts/useapi.js
+++ src/public/scripts/useapi.js
@@ -133,6 +133,32 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(data => {
                 DELETEStatusDiv.textContent = `Employee deleted: ${data.firstname} ${data.lastname}`;
+                
         })
     });
 });
+function hasTouchSupport() {//test device have touch support( usually phone have it)
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+  }
+document.addEventListener('DOMContentLoaded', () => {
+            const testDiv = document.getElementById('device test');
+            testDiv.textContent = navigator.userAgent;
+            if (hasTouchSupport()) {//mobile
+                testDiv.textContent = hasTouchSupport() +  ' This is mobile';
+                document.getElementById('mobile-content').style.display = 'block';
+                document.getElementById('desktop-content').style.display = 'none';
+            } else {
+                testDiv.textContent = hasTouchSupport()+ ' This is Desktop ';
+                // console.log("Desktop device detected");
+                document.getElementById('mobile-content').style.display = 'none';
+                document.getElementById('desktop-content').style.display = 'block';
+              }
+
+            // if (isMobile) {
+            //     document.getElementById('mobile-content').style.display = 'block';
+            //     document.getElementById('desktop-content').style.display = 'none';
+            // } else {
+            //     document.getElementById('mobile-content').style.display = 'none';
+            //     document.getElementById('desktop-content').style.display = 'block';
+            // }
+        });
